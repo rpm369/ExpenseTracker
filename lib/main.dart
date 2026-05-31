0cbdb029ddaeaa2e6be5ccbe26d23752036053b9
rpm369 <963rpm@gmail.com>
2026-05-31 16:32:09 +0530

    Init

---
+++ lib/main.dart
@@ -0,0 +1,13 @@
+import 'package:expense_tracker/Routes/LoginScreen.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(debugShowCheckedModeBanner: false, home: LoginScreen());
+  }
+}
